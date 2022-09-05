4775a1d6ee3e7342dd5502f0fa0ea099ac39e67a
PrasadPrjc <prasad.da@aol.com>
2022-09-05 09:32:20 -0700

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-import logo from '../../logo.svg';
+// import logo from '../../logo.svg';
 import './navbar.css';
 
 const Navbar = () => {
@@ -9,9 +9,9 @@ const Navbar = () => {
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
-        <div className="gpt3__navbar-links_logo">
-          <img src={logo} />
-        </div>
+         <div className="gpt3__navbar-links_logo">
+       
+        </div> 
         <div className="gpt3__navbar-links_container">
           <p><a href="#home">Home</a></p>
           <p><a href="#wgpt3">What is GPT3?</a></p>
@@ -31,11 +31,11 @@ const Navbar = () => {
         {toggleMenu && (
         <div className="gpt3__navbar-menu_container scale-up-center">
           <div className="gpt3__navbar-menu_container-links">
-            <p><a href="#home">Home</a></p>
-            <p><a href="#wgpt3">What is GPT3?</a></p>
-            <p><a href="#possibility">Open AI</a></p>
-            <p><a href="#features">Case Studies</a></p>
-            <p><a href="#blog">Library</a></p>
+            <p><a href="#home"> </a></p>
+            <p><a href="#wgpt3">NextLevelSoftwareLabs</a></p>
+            <p><a href="#possibility"></a></p>
+            <p><a href="#features"></a></p>
+            <p><a href="#blog"></a></p>
           </div>
           <div className="gpt3__navbar-menu_container-links-sign">
             <p>Sign in</p>
